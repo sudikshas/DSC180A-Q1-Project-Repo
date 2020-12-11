b18e2370ea47bcc24fda333af3a5c516d01549b5
sudiksha sarvepalli <ssarvepa@dsmlp-login.ucsd.edu>
2020-12-11 15:04:55 -0800

    final code

--- run.py
+++ run.py
@@ -29,14 +29,11 @@ sys.path.insert(0, 'src') # add library code to path
 
 
 #get config file path names
-#data_ingest_params = 'config/test_data_params.json'
 data_params = 'config/data_input_params.json'
 file_params = 'config/data_files.json'
 results_paths = 'config/results_paths.json'
 test_results = 'config/test_results_file.json'
-#test_params = 'test/testdata/test_params.json'
-#classifier_params = 'config/classifier_params.json'
-#cnn_params = 'config/test_results_file.json'
+
 
 
 
@@ -57,27 +54,23 @@ def main(targets):
     if 'generate_image' in targets: #data
         #access files for the data
         files = load_params(file_params)
-        inputs = load_params(data_params)
-        results = load_params(results_paths)
-        
         #perform etl
-        img = get_input_image(files["data_dir"], files["data_file"], inputs["object_categories"])
-     
-        cv2.imwrite(inputs["image_out_path"], img)
+        img = get_input_image(files["data_dir"], files["data_file"], files["object_categories"])
+        cv2.imwrite(files["image_out_path"], img)
        
 
 
     if 'test' in targets:
-        inputs= load_params(data_params)
+
         results = load_params(results_paths)
-        #
-        save_path = load_params(test_results)["out_dir"]
+        
+        save_path = results["out_dir"]
         
         if not os.path.exists(save_path):
             os.makedirs(save_path) 
         print("finish making data directory")
     
-        run_models(inputs["image_out_path"],results["cam_img"],results["gb_dir"], results['cam_dir'])
+        run_models(results["image_out_path"],results["cam_img"],results["gb_dir"], results['cam_dir'])
         print("finished running model")
 
         
